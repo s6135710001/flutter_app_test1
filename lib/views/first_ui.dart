ad3e12cf796c18d56c87219c5488367d7bb3ff98
s6135710001 <s6135710001@sau.ac.th>
2021-09-10 21:03:19 +0700

    Code V.1

---
+++ lib/views/first_ui.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class FirstUI extends StatelessWidget {
+  const FirstUI({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: Text(
+          'First UI >_<',
+        ),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      body: Center(
+        child: Text(
+          'Anothai Srisangthum',
+          style: TextStyle(
+            fontSize: 35.0,
+            color: Colors.red,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
